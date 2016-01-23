cb483575e026f1fccf1a3646afebe449dd7774a5
rauschma <axel@rauschma.de>
2016-01-23 08:53:43 +0100

    Bug fix: missing parameter

--- impl/promise_timeout.js
+++ impl/promise_timeout.js
@@ -1,6 +1,6 @@
 export function timeout(ms, promise) {
     return new Promise(function (resolve, reject) {
-        promise.then(resolve);
+        promise.then(resolve, reject);
         setTimeout(function () {
             reject(new Error('Timeout after '+ms+' ms'));
         }, ms);
