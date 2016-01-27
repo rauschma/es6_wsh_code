c0c9f6f91275f327091224991ffe13a3ad33650b
Axel Rauschmayer <axel@rauschma.de>
2016-01-27 19:37:29 +0100

    Better test for private data example.

--- test/weakmaps_private_data_test.js
+++ test/weakmaps_private_data_test.js
@@ -1,5 +1,4 @@
-/* global suite */
-/* global test */
+/* global suite,test */
 
 /* EXERCISE:
    - Copy support/weakmaps_private_data_es5.js to impl/weakmaps_private_data_es6.js
@@ -18,4 +17,11 @@ test('Empty StringBuilder', () => {
 });
 test('Adding strings to a StringBuilder', () => {
     assert.equal(new StringBuilder().add('foo').add('bar').toString(), 'foobar');
+});
+test('Rule out singleton implementations', () => {
+    const sb1 = new StringBuilder().add('sb1');
+    const sb2 = new StringBuilder().add('sb2');
+    
+    assert.equal(sb1.toString(), 'sb1');
+    assert.equal(sb2.toString(), 'sb2');
 });
