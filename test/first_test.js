60f3a638cc9d9cfe3d31fa90b14188b152e7c5ed
rauschma <axel@rauschma.de>
2016-03-31 13:10:02 +0200

    First exercise is now a unit test.

---
+++ test/first_test.js
@@ -0,0 +1,17 @@
+/* global suite,test */
+
+/* EXERCISE:
+   - Run this test (it should initially fail):
+     npm test test/first_test.js
+   - Create a file impl/first.js so that the test passes
+*/
+
+import assert from 'assert';
+
+import {ANSWER} from '../impl/first'
+
+suite('first_test');
+
+test('Does the actual result equal the expected result?', () => {
+    assert.strictEqual(ANSWER, 42); // actual, expected
+});
