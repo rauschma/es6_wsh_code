e9ded9e1bad7c730925f5adc0dcc72d772e4a361
rauschma <axel@rauschma.de>
2016-03-31 13:08:52 +0200

    Tests for both named exports and default exports.

---
+++ test/export_default_test.js
@@ -0,0 +1,13 @@
+/* global suite,test */
+
+// EXERCISE: create the module impl/export_default.js so that this test passes
+
+import assert from 'assert';
+
+import func from '../impl/export_default';
+
+suite('export_default_test');
+
+test('func()', () => {
+    assert.equal(func(), 'hello');
+});
---
+++ test/export_named_test.js
@@ -0,0 +1,13 @@
+/* global suite,test */
+
+// EXERCISE: create the module impl/export_named.js so that this test passes
+
+import assert from 'assert';
+
+import {func} from '../impl/export_named';
+
+suite('export_named_test');
+
+test('func()', () => {
+    assert.equal(func(), 'hello');
+});
