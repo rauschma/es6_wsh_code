431ea40335b562538058bac706e27cb46a9d26a1
Axel Rauschmayer <axel@rauschma.de>
2016-04-01 11:16:40 +0200

    Fixed tests for timeout()

--- test/promise_timeout_test.js
+++ test/promise_timeout_test.js
@@ -1,7 +1,6 @@
-/* global suite */
-/* global test */
+/* global suite,test */
 
-// EXERCISE: implement promise_timeout.js
+// EXERCISE: implement impl/promise_timeout.js
 // Imported: function timeout(ms, promise) { ··· }
 
 import assert from 'assert';
@@ -10,35 +9,32 @@ import { timeout } from '../impl/promise_timeout';
 
 suite('promise_timeout_test');
 
-test('Waiting longer than timeout must cause error', done => {
-    timeout(500, delay(1000))
+test('Waiting longer than timeout must cause error', () => {
+    return timeout(500, delay(1000))
     .then(() => {
         // No timeout occurred: error!
         throw new Error('Promise did not time out with error');
     })
     .catch(() => {
         // Everything is OK
-        done();
     });
 });
-test('Must pass on Promise result', done => {
-    timeout(0, Promise.resolve(123))
+test('Must pass on Promise result', () => {
+    return timeout(0, Promise.resolve(123))
     .then(result => {
         assert.strictEqual(result, 123);
-        done();
     })
     .catch(() => {
         throw new Error('Unexpected rejection');
     });
 });
-test('Must pass on Promise rejection', done => {
-    timeout(0, Promise.reject(123))
+test('Must pass on Promise rejection', () => {
+    return timeout(0, Promise.reject(123))
     .then(result => {
         throw new Error('Unexpected resolution');
     })
     .catch(reason => {
         assert.strictEqual(reason, 123);
-        done();
     });
 });
 
