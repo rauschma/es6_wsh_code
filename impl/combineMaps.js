c64126e1d0cfaf4ff71a8e2013692cbcaa983b21
Axel Rauschmayer <axel@rauschma.de>
2016-04-02 23:00:31 +0200

    Explained what steps were performed implicitly when combining Maps.

--- impl/combineMaps.js
+++ impl/combineMaps.js
@@ -1,3 +1,12 @@
 export default function (map1, map2) {
     return new Map([...map1, ...map2]);
 }
+
+/*
+// The steps, more explicitly:
+export default function (map1, map2) {
+    const arr1 = [...map1];
+    const arr2 = [...map2];
+    return new Map([...arr1, ...arr2]);
+}
+*/
